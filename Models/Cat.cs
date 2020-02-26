d04ccba96bf47100c0792bde8947f4afe49d11f1
Jieun Kang and Keturah Howard <jieun+keturah@>
2020-02-26 13:23:35 -0800

    initial commit and environment set up

---
+++ Models/Cat.cs
@@ -0,0 +1,7 @@
+namespace AnimalShelter.Models
+{
+  public class Cats
+  {
+
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using AnimalShelter.Models;
+
+namespace AnimalShelter
+{
+  public class Program
+  {
+    static void Main()
+    {
+
+    }
+  }
+}
